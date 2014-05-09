908d0071564eeb6fc2bf16cca39080c8463584bd
Protoneer <krugercreations@gmail.com>
2014-05-09 22:26:55 +1200

    Initial structure and cleanup

--- www/WebServer.py
+++ www/WebServer.py
@@ -1,5 +1,6 @@
 import cgi
 import serial
+import re
 
 # Serial Config
 #serialPort = '/dev/ttyAMA0'
@@ -9,7 +10,7 @@ serilaBaudRate = 9600
 #Webserver Config
 wwwPort = 91 
 wwwHostName = ''
-wwwHistoryFileName = 'history.txt' # Stores results from commands run
+wwwHistoryFileName = 'WWW/history.txt' # Stores results from commands run
 
 
 
@@ -18,9 +19,15 @@ from http.server import HTTPServer
 #from SimpleHTTPServer import SimpleHTTPRequestHandler
 from http.server import SimpleHTTPRequestHandler
 
+def writeToFile(filename,stringToAppend):
+    f = open(filename,'a')
+    f.write(re.sub(r"\r", "", stringToAppend))
+    f.close()
+
+
 class MyHandler(SimpleHTTPRequestHandler):
+
   def do_POST(self):
-    print(self.path)
     if self.path == '/arduino':
       form = cgi.FieldStorage(fp=self.rfile, headers=self.headers,environ={'REQUEST_METHOD':'POST'})
       code = form['code'].value
@@ -35,28 +42,49 @@ class MyHandler(SimpleHTTPRequestHandler):
       
       from time import sleep
       sleep(0.02)
-      # write to file
-      f = open(wwwHistoryFileName,'a')
-      
+
       out = ''
       while arduino.inWaiting() > 0:
 	  
         out = out + arduino.readline().decode(encoding='UTF-8')
-        sleep(0.02)
+        sleep(0.02)        
 
-        #f.write(arduino.readline())
-        #if addReturnChar == 'on' :
-          #f.write('\r\n')
-      f.write(out)
-      f.close()
-      
+      writeToFile(wwwHistoryFileName,out)
+	  
       self.send_response(200)
       self.send_header('Content-type', 'text/html')
       self.send_header('Access-Control-Allow-Origin', '*')
       self.end_headers()
       return
-    return self.do_GET()
+    return do_GET()
 
+  def do_GET(self):
+      if wwwHistoryFileName in self.path:
+        print("Reading...")
+        out = ''
+        from time import sleep
+        while arduino.inWaiting() > 0:
+          out = out + arduino.readline().decode(encoding='UTF-8')
+          sleep(0.02)        
+        writeToFile(wwwHistoryFileName,out)
+
+        f = self.send_head()
+        if f:
+            try:
+                self.copyfile(f, self.wfile)
+            finally:
+                f.close()	  
+      else:
+        f = self.send_head()
+        if f:
+            try:
+                self.copyfile(f, self.wfile)
+            finally:
+                f.close()	  
+
+    
+				
 print("starting up....")
+print("Hosted @ " + wwwHostName + " Port : " + str(wwwPort))
 arduino = serial.Serial(serialPort, serilaBaudRate, timeout=1)
 server = HTTPServer((wwwHostName, wwwPort), MyHandler).serve_forever()
